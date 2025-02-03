04166e23d420ef3cebc7ac254ad13848aba39c19
Alex Ross <alexross4381@gmail.com>
2025-02-03 14:50:48 -0500

    Added the Team Class to Team.java, along with the getters and setters for each variable

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("March Madness");
 
+    Team t1 = new Team("Duke",.904,8.11,6.99,2,88.2,123.9,1500);
+    System.out.println(t1);
 }
 
 
