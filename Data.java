a826f0b7a9bf4fed14dace01e79d13df96e89a20
Alex Ross <alexross4381@gmail.com>
2025-02-05 02:09:09 -0500

    added the fileURL and savePath

--- Data.java
+++ Data.java
@@ -1,2 +1,14 @@
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 public class Data {
+    String fileURL = "http://barttorvik.com/2025_team_results.csv";
+    String savePath = "2025_team_results.csv";
+
+
+
+
 }
